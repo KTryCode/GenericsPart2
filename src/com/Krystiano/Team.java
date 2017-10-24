a61b02570a803a7a92ee0549466a8274fe14dc33
KTryCode <32237739+KTryCode@users.noreply.github.com>
2017-10-24 23:24:31 +0200

    0.4

--- src/com/Krystiano/Team.java
+++ src/com/Krystiano/Team.java
@@ -10,4 +10,46 @@ public class Team {
     int tied=0;
 
     private ArrayList<Player> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(Player player){
+        if (members.contains(player)){
+            System.out.println(player.getName() + " is already on this team");
+            return false;
+        }
+        else {
+            members.add(player);
+            System.out.println(player.getName()+" successfully added");
+            return true;
+        }
+    }
+
+    public int numPlayers(){
+        return this.members.size();
+    }
+
+    public void matchResult(Team opponent, int ourScore, int theirScore){
+        if(ourScore > theirScore){
+            won++;
+        } else if(ourScore==theirScore){
+            tied++;
+        } else {
+            lose++;
+        }
+        played++;
+        if(opponent != null){
+            opponent.matchResult(null,theirScore,ourScore);
+        }
+    }
+
+    public int ranking(){
+        return (won*2 + tied);
+    }
 }
