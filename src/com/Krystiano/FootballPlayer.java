b66928cf8431f42ee13d09048ce03db58779294c
KTryCode <32237739+KTryCode@users.noreply.github.com>
2017-10-24 23:14:03 +0200

    0.2
    Players classes definied

---
+++ src/com/Krystiano/FootballPlayer.java
@@ -0,0 +1,7 @@
+package com.Krystiano;
+
+public class FootballPlayer extends Player {
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ src/com/Krystiano/SoccerPlayer.java
@@ -0,0 +1,7 @@
+package com.Krystiano;
+
+public class SoccerPlayer extends Player {
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
